fb48498e94a17eecd4f2e9d5e1a018b8360d2343
khoirulanam <khoirulanam792022@gmail.com>
2025-09-06 05:03:43 +0700

    kj

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
-/// <reference types="vite/client" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// GANTI 'LARAS' dengan NAMA REPO kamu
+export default defineConfig({
+  base: '/LARAS/', 
+  plugins: [react()],
+})
