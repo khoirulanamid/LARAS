61daa8068ec748c36c7593336300ffb06157028d
khoirulanam <khoirulanam792022@gmail.com>
2025-09-05 11:39:36 +0700

    update

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useDarkMode } from "../lib/useDarkMode";
+import { useDarkMode } from "../lib/userDarkMode";
 
 function IconSun(props: React.SVGProps<SVGSVGElement>) {
   return (
